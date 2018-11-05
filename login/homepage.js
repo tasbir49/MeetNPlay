4fddfcf0adc1b43a615f835a723ae73837397f19
baojame2 <baojame2@teach.cs.utoronto.ca>
2018-11-05 15:40:51 -0500

    made a login page more formal

---
+++ login/homepage.js
@@ -0,0 +1,26 @@
+"use strict"
+
+const loginForm = document.querySelector('#loginForm')
+loginForm.addEventListener('submit',login);
+
+
+
+function login(e){
+  e.preventDefault();
+
+
+  if (validLogin(loginForm.firstElementChild.value,loginForm.firstElementChild.nextElementSibling.value)){
+    console.log("hi");
+    window.location.href = "../forum/forum.html";
+  } else{
+    console.log("wrong");
+    alert("wrong login please try again");
+  }
+}
+
+function validLogin(user,pass){
+  const rightUser = "Test"
+  const rightPass = "Test"
+
+  return rightUser == user && rightPass == pass
+}
