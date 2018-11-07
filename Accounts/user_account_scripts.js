f9f7b247fdeffd85fd4a6bd246805618c930b574
tasbir rahman <tasbir49@gmail.com>
2018-11-07 17:42:29 -0500

    Added edit and pass change "functionality"

--- Accounts/user_account_scripts.js
+++ Accounts/user_account_scripts.js
@@ -15,65 +15,128 @@ const usrSearchForm = document.querySelector("#userSearchInput");
 
 //event listeners
 let i = 0;
-for (i = 0; i < editButtons.length; i++) {
-    editButtons[i].addEventListener("click", bringUpInfoPrompt);
+
+if(editButtons) {
+    for (i = 0; i < editButtons.length; i++) {
+        editButtons[i].addEventListener("click", editInfoCallback);
+    }
 }
 
+if(passChangeButton) {
+    passChangeButton.addEventListener("click", openPassPromptCallback);
+}
+if(infoChange){
+    infoChange.addEventListener("click", closeInfoPromptCallback);
+}
 
-passChangeButton.addEventListener("click", bringUpPassPrompt);
-infoChange.addEventListener("click", bringDownInfoPrompt);
-passChange.addEventListener("click", bringDownPassPrompt);
-userNotFound.addEventListener("click", bringDownUserPrompt);
+if(passChange){ 
+    passChange.addEventListener("click", closePassPromptCallback);
+    passChange.addEventListener("submit", changePassCallback);
+}
 
-usrSearchForm.addEventListener("submit", searchUser);
+userNotFound.addEventListener("click", closeUserNotFoundPromptCallback);
+
+usrSearchForm.addEventListener("submit", searchUserCallback);
 
 
 
-//normally this function would search a database of users in back end 
+//normally this function would search a database of users in back end /
+//and redirect to the found user's page
 //but for front end, itll just not find anything
-function searchUser(e) {
+function searchUserCallback(e) {
     e.preventDefault();
-    bringUpUserPrompt(e);
+    openUserNotFoundPromptCallback(e);
     
 }
 
-function bringUpUserPrompt(e) {
+
+
+function openUserNotFoundPromptCallback(e) {
     userNotFound.style.display = "block";
 }
 
-
-function bringUpInfoPrompt(e) {
-    infoChange.style.display = "block";
-    
+function closeUserNotFoundPromptCallback(e) {
+    userNotFound.style.display = "none";
 }
 
 
-function bringUpPassPrompt(e) {
+
+function openInfoPromptCallback(e) {
+    
+    infoChange.style.display = "block";
+}
+function closeInfoPromptCallback(e) {
+    if(e.target.classList.contains("closeBox") || e.target.classList.contains("dialogContainer"))
+    {
+        cleanInfoPrompt();
+    }
+}
+
+function cleanInfoPrompt() {
+    infoChange.querySelector("textarea").value = "";
+    infoChange.style.display = "none";
+}
+
+
+
+function openPassPromptCallback(e) {
+    
     passChange.style.display = "block";
     
 }
 
-function bringDownInfoPrompt(e) {
-    if(e.target.classList.contains("closeBox") || e.target.classList.contains("dialogContainer"))
-    {
-        infoChange.querySelector("textarea").value = "";
-        infoChange.style.display = "none";
-    }
-}
-
-function bringDownUserPrompt(e) {
-    userNotFound.style.display = "none";
-}
-
-function bringDownPassPrompt(e) {
+function closePassPromptCallback(e) {
     
      if(e.target.classList.contains("closeBox") || e.target.classList.contains("dialogContainer"))
         {
-            let textInputs = passChange.querySelectorAll("input[type=\"text\"]");
-            let i = 0;
-            for(i = 0; i < textInputs.length; i++) {
-                textInputs[i].value = "";
-            }
-            passChange.style.display = "none";
+            cleanPassPrompt();
         }    
+}
+
+function cleanPassPrompt() {
+    let textInputs = passChange.querySelectorAll("input[type=\"text\"]");
+    let i = 0;
+    for(i = 0; i < textInputs.length; i++) {
+        textInputs[i].value = "";
+    }
+    passChange.style.display = "none";
+}
+
+
+//normally this would update info to the backend as well
+function editInfoCallback(e) {
+    infoEditSetup(e.target.parentNode);    
+    openInfoPromptCallback(e)
+}
+
+//function using closures to allow access to infoSection from infoEditCallback
+function infoEditSetup(infoSectionNode) {
+    
+    //Function to edit fields in account page
+    function infoEditCallback(e) {
+       e.preventDefault();
+       console.log(this);
+       let newInfoContent = e.target.parentNode.querySelector("#newInfo").value
+       let infoContentArea = infoSectionNode.querySelector(".userInfoSectionContent");
+       if(infoContentArea.firstChild) {
+           infoContentArea.removeChild(infoContentArea.firstChild);
+        }
+        let newText = document.createTextNode(newInfoContent);
+        infoContentArea.appendChild(newText);
+        
+        //dont want unnecessary listener
+        infoChange.removeEventListener("submit", infoEditCallback);
+        cleanInfoPrompt();
+    }
+    
+    //listener only needed for setup
+    infoChange.addEventListener("submit", infoEditCallback);
+
+}
+
+//normally the hash is updated to db and there are checks made with old
+//password
+function changePassCallback(e) {
+    e.preventDefault();
+    cleanPassPrompt();
 }
