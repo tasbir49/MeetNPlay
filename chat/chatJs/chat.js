cec3ffd859a134e076a8499bc509c13949dd9a59
baojame2 <baojame2@teach.cs.utoronto.ca>
2018-12-04 17:30:49 -0500

    maaaaan i wanan sleppe

--- chat/chatJs/chat.js
+++ chat/chatJs/chat.js
@@ -1,12 +1,9 @@
 let socket = io.connect();
 $("form#chat").submit(function(e) {
   e.preventDefault();
-
   socket.emit("send message", $(this).find("#msg_text").val(), function() {
     $("form#chat #msg_text").val("");
-
   });
-
 });
 socket.on("update messages", function(msg){
   let final_message = $("<p />").text(msg);
