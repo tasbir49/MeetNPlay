19b8af99b20d5e2fdd3ff0e322b99578d627c3be
baojame2 <baojame2@teach.cs.utoronto.ca>
2018-12-05 17:22:46 -0500

    added igdb server calls

--- final/server.js
+++ final/server.js
@@ -88,7 +88,7 @@ app.route('/login')
 	.get(sessionChecker, (req, res) => {
 		res.sendFile(__dirname + '/public/login.html')
 	})
-    
+
 //getting a post page (NOT THE JSON, THE ACTUAL WEB PAGE)
 app.get('/post/view/:id', authenticate, (req, res) => {
     const id = req.params.id
@@ -96,7 +96,7 @@ app.get('/post/view/:id', authenticate, (req, res) => {
         return res.status(404).send("404 NOT FOUND SORRY")
     }
 
-    
+
     Post.findById(id).then((post) => {
 
         if(!post) {
@@ -108,7 +108,7 @@ app.get('/post/view/:id', authenticate, (req, res) => {
         const formattedLocation = encodeURI(post.meetLocation); //for google map search
         let retObj = {
             user: {
-                    name: req.session.username, 
+                    name: req.session.username,
                     isAdmin: req.session.isAdmin,
                     profilePicUrl: req.session.profilePicUrl,
                     canEdit: false
@@ -120,18 +120,18 @@ app.get('/post/view/:id', authenticate, (req, res) => {
         if(req.session.username == post.creator || req.session.isAdmin) {//these guys can edit parts of the post and add user
             retObj.user.canEdit = true
             res.render("post_view.hbs", retObj)
-            
+
         } else if(post.members.includes(req.session.username)) {//only members of post can view
             res.render("post_view.hbs", retObj)
-            
+
         } else {
             return res.status(403).send("YOU DONT HAVE PERMISSION TO ACCESS THIS RESOURCE")
         }
     }).catch((error)=> {
         res.status(400).send(error)
     })
-    
-})   
+
+})
 
 //getting a post's edit page
 app.get('/post/edit/:id', authenticate, (req, res) => {
@@ -140,66 +140,66 @@ app.get('/post/edit/:id', authenticate, (req, res) => {
         return res.status(404).send("404 NOT FOUND SORRY")
     }
 
-    
+
     Post.findById(id).then((post) => {
 
         if(!post) {
             res.status(404).send("404 NOT FOUND SORRY")
         }
-        
+
         let retObj = {
             user: {
-                    name: req.session.username, 
+                    name: req.session.username,
                     isAdmin: req.session.isAdmin,
                     profilePicUrl: req.session.profilePicUrl,
             },
             isEdit: true, //so we can recycle the post-edit page as a make-post page
             post: post
         }
-        
+
         if(req.session.username == post.creator || req.session.isAdmin) {//these guys can edit parts of the post and add user
             res.render("post_edit.hbs", retObj)
-            
+
         } else {
             return res.status(403).send("YOU DONT HAVE PERMISSION TO ACCESS THIS RESOURCE")
         }
     }).catch((error)=> {
         res.status(400).send(error)
     })
-    
-})   
+
+})
 
 
 //getting a page to make post
 app.get('/post/make', authenticate, (req, res) => {
         let retObj = {
             user: {
-                    name: req.session.username, 
+                    name: req.session.username,
                     isAdmin: req.session.isAdmin,
                     profilePicUrl: req.session.profilePicUrl,
             },
             isEdit: false
         }
-        res.render("post_edit.hbs", retObj)  
-})   
+        res.render("post_edit.hbs", retObj)
+})
 
-//creates a new post, assuming a json body 
-//that matches the schema , 
+//creates a new post, assuming a json body
+//that matches the schema ,
 //using req.sessionuser for crearor
 //This means creator SHOULD NOT be in the request body
 app.post('/post/api/create', authenticate, (req, res)=> {
     let templatePost = req.body
     templatePost.creator = req.session.username
-    templatePost.profilePicUrl = req.session.profilePicUrl 
+    templatePost.profilePicUrl = req.session.profilePicUrl
     const post = new Post(templatePost)
     post.save().then((result)=> {
         res.redirect('/post/view/' + result.id.toString())
-    
+
     }).catch((error)=>{
         res.status(400).send(error)
     })
 
-    
+
 })
 
 //editing a post, assuming json body has all fields
@@ -215,28 +215,28 @@ app.patch('/post/api/edit/:id', authenticate, (req, res) => {
         if(!post) {
             res.status(404).send("404 NOT FOUND SORRY")
         }
-        
+
         let retObj = {
             user: {
-                    name: req.session.username, 
+                    name: req.session.username,
                     isAdmin: req.session.isAdmin,
                     profilePicUrl: req.session.profilePicUrl,
             },
         }
-        
+
         if(req.session.username == post.creator.name || req.session.isAdmin) {//these guys can edit parts of the post and add user
             post.set(req.body)
             post.save().then( (post) => {res.redirect("/post/" + post.id.toString())}
             ).catch((error) => {return res.status(400).send(error)})
-            
+
         } else {
             return res.status(403).send("YOU DONT HAVE PERMISSION TO ACCESS THIS RESOURCE")
         }
     }).catch((error)=> {
         res.status(400).send(error)
     })
-    
-})   
+
+})
 
 
 
@@ -248,16 +248,16 @@ app.get('/users/:username', authenticate, (req, res) => {
     User.findOne({name: username}).then((user) => {
         let retObj = {
                 user: {
-                        name: req.session.username, 
+                        name: req.session.username,
                         isAdmin: req.session.isAdmin,
                         profilePicUrl: req.session.profilePicUrl,
-                        canEdit: false, 
-                        isJustOwner: false 
+                        canEdit: false,
+                        isJustOwner: false
                     },
-                userDetails : user, //header uses the "user" field to display profile pic and link to own profile page, so the details for this page are put in this field instead 
+                userDetails : user, //header uses the "user" field to display profile pic and link to own profile page, so the details for this page are put in this field instead
                 formattedMemberSince: user.memberSince.toISOString().substring(0, 10)
             }
-            
+
         if(!user) {
             res.status(404).send("404 NOT FOUND SORRY")
         }
@@ -268,14 +268,14 @@ app.get('/users/:username', authenticate, (req, res) => {
         else if(req.session.username == username) {//these guys can edit parts of the post and add user
             retObj.user.canEdit = true
             retObj.user.isJustOwner = true
-        } 
+        }
         res.render("user_profile.hbs", retObj)
-        
+
     }).catch((error)=> {
         res.status(404).send("404 NOT FOUND SORRY")
     })
-    
-})  
+
+})
 
 
 
@@ -299,7 +299,7 @@ app.post('/reports/api/create', (req, res)=> {
     }).catch((error)=>{
         res.status(400).send(error)
     })
-    
+
 })
 
 
@@ -317,7 +317,7 @@ app.post('/reports/api/close/:id', (req, res)=> {
     }).catch((error)=>{
         res.status(400).send(error)
     })
-    
+
 })
 
 //need to implement pagination with this
@@ -326,13 +326,13 @@ app.get('/reports', authenticate, (req,res)=> {
         Report.find({isClosed:false}).sort({date: -1}).then((reports)=>{
             res.render("reports.hbs", {
                 user: {
-                        name: req.session.username, 
+                        name: req.session.username,
                         profilePicUrl: req.session.profilePicUrl
                 },
                 reports: reports
-                    
+
             })
-            
+
         }).catch((error)=>{
             res.status(403).send(error)
         })
@@ -341,15 +341,15 @@ app.get('/reports', authenticate, (req,res)=> {
     }
 })
 
-//homepage 
+//homepage
 app.get('/home', (req, res) => {
 	// check if we have active session cookie
 	if (req.session.username) {
 		//res.sendFile(__dirname + '/public/dashboard.html')
 		res.render('homepage.hbs', {
 			user:{
-                name: req.session.username, 
-                isAdmin: req.session.isAdmin, 
+                name: req.session.username,
+                isAdmin: req.session.isAdmin,
                 profilePicUrl: req.session.profilePicUrl},
             headerTitle: "HOME"
 		})
@@ -397,7 +397,37 @@ app.get('/logout', (req, res) => {
 	})
 })
 
+app.get('/igdb/',(req,res)=> {
+ igdb_client.games({
+	 fields: "name,id",
+	 limit:10
+  }).then(response=>{
+	 console.log(response.body.length);
+	 res.send(response.body.sort((a,b)=>{
+		 let x = a.name.toLowerCase()
+		 let y = b.name.toLowerCase()
+		 if(x<y) {return -1}
+		 if(x>y) {return 1}
+	 }));
+ }).catch(error=>{
+	 res.status(400).send(error)
+ })
+})
 
+app.get('/igdb/:name',(req,res)=>{
+
+	igdb_client.games({
+		fields:"name",
+		limit:10,
+		filters: {"name-prefix": req.params.name,
+							"version_parent-not_exists":1}
+	}).then(response=>{
+		console.log(req.params.name);
+		res.send(response.body)
+	}).catch(error=>{
+		res.status(400).send(error)
+	})
+})
 
 
 
@@ -419,5 +449,3 @@ app.post('/users', (req, res) => {
 app.listen(port, () => {
 	log(`Listening on port ${port}...`)
 });
-
-
