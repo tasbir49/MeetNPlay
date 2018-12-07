411eb32b391aaaae1e8c28705ebaaa121a3b1026
tasbir rahman <tasbir49@gmail.com>
2018-12-07 18:59:33 -0500

    fix misplacement

--- final/models/model.js
+++ final/models/model.js
@@ -123,11 +123,11 @@ const CommentSchema = new mongoose.Schema({
 const PostSchema = new mongoose.Schema({//most of these defaults are for testing purposes
 	creator : {
         type: mongoose.Schema.Types.ObjectId, ref: "User",
-        default: "MeetNPlay Meet"
         required: true
     },
     title: {
         type: String,
+        default: "MeetNPlay Meet"
         required: true
     },
     keywords : {
